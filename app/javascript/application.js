289bfdb86d523c7806e25535a80ffd760c24d008
Palash Bera <palashbera1234@gmail.com>
2022-06-07 22:39:56 +0530

    Adding mobile navbar with hamburger

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$(document).on("click", ".hamburger, .overlay", function (event) {
+  $(".sidebar").toggleClass("active");
+  $(".overlay").toggleClass("active");
+});
+
+$(document).keyup(function(e) {
+  if (e.key === "Escape" && $(".overlay").hasClass("active")) {
+    $(".sidebar").toggleClass("active");
+    $(".overlay").toggleClass("active");
+  }
+});
